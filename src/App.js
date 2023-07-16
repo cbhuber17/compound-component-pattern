57428cf27df9b948e152f9bf37e6477212bbf9a2
Colin Huber <cbhuber@gmail.com>
2023-07-15 19:26:02 -0600

    feat: compound component react pattern for a counter.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,31 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
+      {/* <Counter
         iconIncrease="+"
         iconDecrease="-"
         label="My NOT so flexible counter"
         hideLabel={false}
         hideIncrease={false}
         hideDecrease={false}
-      />
+      /> */}
+
+      <Counter>
+        {/* Add or remove components as needed */}
+        <Counter.Decrease icon="-" />
+        <Counter.Count />
+        <Counter.Increase icon="+" />
+        <Counter.Label>My counter</Counter.Label>
+      </Counter>
+
+      <div>
+        <Counter>
+          {/* Add or remove components as needed */}
+          <Counter.Increase icon="+" />
+          <Counter.Count />
+          <Counter.Label>My second counter</Counter.Label>
+        </Counter>
+      </div>
     </div>
   );
 }
